276d2971016ddab75802e3f60bee5ab09fef7105
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-03-03 23:37:36 +0000

    reset button

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -2,8 +2,13 @@ import React, {useState} from 'react';
 
 const Counter = () => {
     const [counter, setCounter] = useState(0);
+
     const increment = () => {
         setCounter(counter + 1);
+    };
+
+    const reset = () => {
+        setCounter(0);
     };
 
     return (
@@ -11,6 +16,7 @@ const Counter = () => {
             <div>{counter}</div>
             <div>
                 <button onClick={increment}>Increment</button>
+                <button onClick={reset}>Reset</button>
             </div>
         </div>);
 
